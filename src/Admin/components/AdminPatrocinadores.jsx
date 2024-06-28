47203cf4886f526780b9ac805ebbd1458b722bee
Guilherme Henrique <gh.dourado77@gmail.com>
2024-06-28 13:29:30 -0300

    feat: ajustando admin

--- src/Admin/components/AdminPatrocinadores.jsx
+++ src/Admin/components/AdminPatrocinadores.jsx
@@ -16,92 +16,74 @@ export function AdminPatrocinadores() {
   };
 
   return (
-    <form className="bg-white rounded-lg p-5 space-y-5">
+    <div className="bg-white rounded-lg p-5 space-y-5">
       <h2 className="text-2xl font-medium">Configurações - PATROCINADORES</h2>
-
-      <div className="grid grid-cols-2">
-        <div>
-          <label className="text-sm grid gap-5 grid-cols-2 mt-4">
-            <div className="flex flex-col space-y-3 gap-1">
-              <span>Logo do Patrocinador</span>
-              <input
-                type="file"
-                accept="image/*"
-                onChange={handleImageChange}
-              />
-            </div>
-            <div>
-              {imagePreview && (
-                <div>
-                  <p className="text-gray-700 mb-2">
-                    Pré-visualização da Imagem:
-                  </p>
-                  <img
-                    src={imagePreview}
-                    alt="Image Preview"
-                    className="rounded-lg object-cover max-h-40"
-                  />
-                </div>
-              )}
-            </div>
-          </label>
-          <div className="grid grid-cols-2 gap-5 mt-4">
-            <label className="flex flex-col gap-1">
-              <span>Texto botão (PT)</span>
-              <input
-                type="text"
-                placeholder="Texto do Botão"
-                className="bg-zinc-50 px-5 py-1 rounded-lg"
-              />
-            </label>
-            <label className="flex flex-col gap-1">
-              <span>Texto botão (EN)</span>
-              <input
-                type="text"
-                placeholder="Button Text"
-                className="bg-zinc-50 px-5 py-1 rounded-lg"
-              />
-            </label>
+      <form className="space-y-5">
+        <label className="text-sm grid grid-cols-2">
+          <div className="flex flex-col space-y-3 gap-1">
+            <span>Logo do Patrocinador</span>
+            <input type="file" accept="image/*" onChange={handleImageChange} />
           </div>
-        </div>
+          <div>
+            {imagePreview && (
+              <div>
+                <p className="text-gray-700 mb-2">
+                  Pré-visualização da Imagem:
+                </p>
+                <img
+                  src={imagePreview}
+                  alt="Image Preview"
+                  className="rounded-lg object-cover max-h-40"
+                />
+              </div>
+            )}
+          </div>
+        </label>
+        <label className="flex flex-col gap-1 text-sm">
+          <span>Nome</span>
+          <input
+            type="text"
+            className="bg-zinc-50 px-5 py-2"
+            placeholder="Nome do Patrocinador"
+          />
+        </label>
+        <button className="bg-black text-white w-full py-1 rounded-lg">
+          Adicionar
+        </button>
+      </form>
 
-        <div className="mt-8 grid gap-5 grid-cols-6">
-          <img
-            src={ImageGalery}
-            alt=""
-            className="w-full h-16 object-cover rounded-lg"
-          />
-          <img
-            src={ImageGalery}
-            alt=""
-            className="w-full h-16 object-cover rounded-lg"
-          />
-          <img
-            src={ImageGalery}
-            alt=""
-            className="w-full h-16 object-cover rounded-lg"
-          />
-          <img
-            src={ImageGalery}
-            alt=""
-            className="w-full h-16 object-cover rounded-lg"
-          />
-          <img
-            src={ImageGalery}
-            alt=""
-            className="w-full h-16 object-cover rounded-lg"
-          />
-          <img
-            src={ImageGalery}
-            alt=""
-            className="w-full h-16 object-cover rounded-lg"
-          />
-        </div>
+      <div className="mt-8 grid gap-5 grid-cols-6">
+        <img
+          src={ImageGalery}
+          alt=""
+          className="w-full h-16 object-cover rounded-lg"
+        />
+        <img
+          src={ImageGalery}
+          alt=""
+          className="w-full h-16 object-cover rounded-lg"
+        />
+        <img
+          src={ImageGalery}
+          alt=""
+          className="w-full h-16 object-cover rounded-lg"
+        />
+        <img
+          src={ImageGalery}
+          alt=""
+          className="w-full h-16 object-cover rounded-lg"
+        />
+        <img
+          src={ImageGalery}
+          alt=""
+          className="w-full h-16 object-cover rounded-lg"
+        />
+        <img
+          src={ImageGalery}
+          alt=""
+          className="w-full h-16 object-cover rounded-lg"
+        />
       </div>
-
-      <button className="bg-black w-full text-white py-2 rounded-lg">
-        Salvar
-      </button>
-    </form>
+    </div>
   );
 }
--- src/components/equipe-components/CardEquipe.jsx
+++ src/components/equipe-components/CardEquipe.jsx
@@ -2,18 +2,18 @@ export function CardEquipe({ data, lang }) {
   return (
     <div className="space-y-3">
       <img
-        src={data.filePath}
-        alt={data.nome}
+        src={data?.filePath}
+        alt={data?.nome}
         className="w-full h-96 object-cover"
       />
       <div className="space-y-1">
-        <h3 className="text-2xl font-semibold">{data.nome}</h3>
+        <h3 className="text-2xl font-semibold">{data?.nome}</h3>
         <div className="space-y-2">
-          <span className="text-[#83BF45]">{data.rede_social}</span>
+          <span className="text-[#83BF45]">{data?.rede_social}</span>
           <div className="w-8 h-1 bg-[#83BF45]" />
         </div>
       </div>
-      <p>{lang === "en" ? data.descricao_en : data.descricao_pt}</p>
+      <p>{lang === "en" ? data?.descricao_en : data?.descricao_pt}</p>
     </div>
   );
 }
