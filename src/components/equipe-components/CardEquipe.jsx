143ea98fc5421392da2d00797284259cb9e14aab
Guilherme Henrique <gh.dourado77@gmail.com>
2024-07-03 10:00:50 -0300

    fix: corrigindo link do instagram

--- src/components/equipe-components/CardEquipe.jsx
+++ src/components/equipe-components/CardEquipe.jsx
@@ -7,9 +7,22 @@ export function CardEquipe({ data, lang }) {
         className="w-full h-96 object-cover"
       />
       <div className="space-y-1">
-        <h3 className="text-2xl font-semibold">{data?.nome}</h3>
+        <h3 className="text-2xl font-">
+          {data?.nome.split(" ")[0]} <br />{" "}
+          <b className="font-bold">
+            {data?.nome.split(" ").slice(1).join(" ")}
+          </b>
+        </h3>
         <div className="space-y-2">
-          <span className="text-[#83BF45]">{data?.rede_social}</span>
+          {data?.reseSocial && (
+            <a
+              href={`http://instagram.com/${data?.reseSocial}`}
+              className="text-[#83BF45]"
+              target="_blank"
+            >
+              @{data?.reseSocial}
+            </a>
+          )}
           <div className="w-8 h-1 bg-[#83BF45]" />
         </div>
       </div>
